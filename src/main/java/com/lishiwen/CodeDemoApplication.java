8f918d9d495c45b92acd8da80a34b29c64a3e8bf
za-lishiwen <shiwen.li@zatech.com>
2022-06-10 15:47:26 +0800

    init

---
+++ src/main/java/com/lishiwen/CodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lishiwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeDemoApplication.class, args);
+    }
+
+}
